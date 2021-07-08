37b669fbdf70c1666bc2230db9c6914f8321cac3
9i-NaimBiswas <naim@9idigitals.in>
2021-07-08 17:59:56 +0600

    investment details api

--- routes/buyInvestmentRouter.js
+++ routes/buyInvestmentRouter.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const Router = express.Router()
 const investModel = require('../model/investModel')
-
+// buy a investment 
 Router.post('/buy-investment', (req, res) => {
    const userId = req.headers['userid']
    const investId = req.headers['investid']
 
    const { investPrice, profitPerHour, durationOfInvest, purhaseTime, expireTime } = req.body
 
-   if (userId && investPrice && profitPerHour && durationOfInvest && purhaseTime && expireTime) {
+   if (userId && investPrice && profitPerHour && purhaseTime && expireTime) {
       const newInvest = new investModel({
          userId: userId,
          investId: investId,
@@ -34,7 +34,23 @@ Router.post('/buy-investment', (req, res) => {
    }
 })
 
-
+// get all investment 
+Router.get('/get-investment-details', (req, res) => {
+   const userId = req.headers['userid']
+   investModel.find({ userId: userId }).then(data => {
+      res.status(200).json({
+         message: "All invest ment history",
+         success: true,
+         data: data
+      })
+   }).catch(err => {
+      res.status(500).json({
+         message: "Some thing went wrong",
+         success: false,
+         err: err,
+      })
+   })
+})
 
 
 module.exports = Router
